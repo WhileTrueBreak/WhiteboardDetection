6682eff853c9c1574694b1811008d8f713b2f81c
Jeffrey Chen <jche0470@student.monash.edu>
2025-05-06 17:23:28 +1000

    added new temporal filtering

--- image_demo.py
+++ image_demo.py
@@ -4,6 +4,7 @@ from place_solver import solve_mask_quad
 from glob import glob
 import numpy as np
 import matplotlib
+import argparse
 import colorsys
 import network
 import config
@@ -12,69 +13,76 @@ import cv2
 import os
 
 matplotlib.use('TkAgg')
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-transform = T.Compose([
-    T.ToTensor(),   
-    T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
-])
-model = network.modeling.deeplabv3plus_mobilenet(num_classes=config.NUM_CLASSES, output_stride=config.OUTPUT_STRIDE)
-if os.path.exists(f'models/cp_{config.MODEL_NAME}_{config.NUM_CLASSES}cls.pth'):
-    print(f'Loading pretrained weights from models/cp_{config.MODEL_NAME}_{config.NUM_CLASSES}cls.pth')
-    model.load_state_dict(torch.load(f'models/cp_{config.MODEL_NAME}_{config.NUM_CLASSES}cls.pth', map_location=device, weights_only=True))
-model.to(device)
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    transform = T.Compose([
+        T.ToTensor(),   
+        T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+    ])
+
+    parser = argparse.ArgumentParser(description='Image Demo')
+    parser.add_argument('--model-path', type=str, default=f'models/cp_{config.MODEL_NAME}_{config.NUM_CLASSES}cls.pth', help='Path to the model .pth file')
+    args = parser.parse_args()
+
+    model = network.modeling.deeplabv3plus_mobilenet(num_classes=config.NUM_CLASSES, output_stride=config.OUTPUT_STRIDE)
+    if os.path.exists(args.model_path):
+        print(f'Loading pretrained weights from {args.model_path}')
+        model.load_state_dict(torch.load(args.model_path, map_location=device, weights_only=True))
+    model.to(device)
 
 
-color_mapping = np.array([colorsys.hsv_to_rgb(i/config.NUM_CLASSES, 1, 1)
-                          for i in range(config.NUM_CLASSES)])
-image_paths = glob("./res/*.*")
-if not image_paths:
-    print("No images found in the './res' folder.")
-    exit()
+    color_mapping = np.array([colorsys.hsv_to_rgb(i/config.NUM_CLASSES, 1, 1)
+                            for i in range(config.NUM_CLASSES)])
+    image_paths = glob("./res/*.*")
+    if not image_paths:
+        print("No images found in the './res' folder.")
+        exit()
 
-os.makedirs('./output', exist_ok=True)
+    os.makedirs('./output', exist_ok=True)
 
-with torch.no_grad():
-    model = model.eval()
-    for img_path in image_paths:
-        # Load image using OpenCV
-        ci = cv2.imread(img_path)
-        if ci is None:
-            print(f"Failed to load image: {img_path}")
-            continue
+    with torch.no_grad():
+        model = model.eval()
+        for img_path in image_paths:
+            # Load image using OpenCV
+            ci = cv2.imread(img_path)
+            if ci is None:
+                print(f"Failed to load image: {img_path}")
+                continue
 
-        ci_rgb = cv2.cvtColor(ci, cv2.COLOR_BGR2RGB)
-        cam_res = ci_rgb.shape
+            ci_rgb = cv2.cvtColor(ci, cv2.COLOR_BGR2RGB)
+            cam_res = ci_rgb.shape
 
-        input_img = cv2.resize(ci_rgb, (config.INPUT_SIZE[1], config.INPUT_SIZE[0]))
-        img_tensor = transform(input_img).unsqueeze(0).to(device)
-        pred = model(img_tensor).max(1)[1].cpu().numpy()[0]
-        pred = np.array(pred)
+            input_img = cv2.resize(ci_rgb, (config.INPUT_SIZE[1], config.INPUT_SIZE[0]))
+            img_tensor = transform(input_img).unsqueeze(0).to(device)
+            pred = model(img_tensor).max(1)[1].cpu().numpy()[0]
+            pred = np.array(pred)
 
-        color_mapping = np.array([colorsys.hsv_to_rgb(i/2, 1, 1) for i in range(2)])
-        pred_full_mask = (pred > 0).astype(int)
+            color_mapping = np.array([colorsys.hsv_to_rgb(i/2, 1, 1) for i in range(2)])
+            pred_full_mask = (pred > 0).astype(int)
 
-        kernel = np.ones((11,11), np.uint8)
-        pred_full_mask = cv2.morphologyEx(pred_full_mask.astype(np.uint8), cv2.MORPH_OPEN, kernel)
-        pred_full_mask = cv2.morphologyEx(pred_full_mask, cv2.MORPH_CLOSE, kernel)
+            kernel = np.ones((11,11), np.uint8)
+            pred_full_mask = cv2.morphologyEx(pred_full_mask.astype(np.uint8), cv2.MORPH_OPEN, kernel)
+            pred_full_mask = cv2.morphologyEx(pred_full_mask, cv2.MORPH_CLOSE, kernel)
 
-        colorized_pred = (color_mapping[pred_full_mask,:]*255).astype('uint8')
-        colorized_pred = cv2.resize(colorized_pred, (cam_res[1], cam_res[0]), interpolation=cv2.INTER_NEAREST)
+            colorized_pred = (color_mapping[pred_full_mask,:]*255).astype('uint8')
+            colorized_pred = cv2.resize(colorized_pred, (cam_res[1], cam_res[0]), interpolation=cv2.INTER_NEAREST)
 
-        # Overlay the original image and the colorized prediction
-        overlay = cv2.addWeighted(ci_rgb, 0.5, colorized_pred, 0.5, 0)
-        frame_out = cv2.cvtColor(overlay, cv2.COLOR_RGB2BGR)
+            # Overlay the original image and the colorized prediction
+            overlay = cv2.addWeighted(ci_rgb, 0.5, colorized_pred, 0.5, 0)
+            frame_out = cv2.cvtColor(overlay, cv2.COLOR_RGB2BGR)
 
-        # mask the vertex image with whiteboard mask
-        pred_cam_res = cv2.resize(pred, (cam_res[1], cam_res[0]), interpolation=cv2.INTER_NEAREST)
-        whiteboard_mask = (pred_cam_res == 1)
-        # whiteboard_full_mask = (pred_cam_res > 0)
-        whiteboard_full_mask = cv2.resize(pred_full_mask, (cam_res[1], cam_res[0]), interpolation=cv2.INTER_NEAREST)
+            # mask the vertex image with whiteboard mask
+            pred_cam_res = cv2.resize(pred, (cam_res[1], cam_res[0]), interpolation=cv2.INTER_NEAREST)
+            whiteboard_mask = (pred_cam_res == 1)
+            # whiteboard_full_mask = (pred_cam_res > 0)
+            whiteboard_full_mask = cv2.resize(pred_full_mask, (cam_res[1], cam_res[0]), interpolation=cv2.INTER_NEAREST)
 
-        shapes = solve_mask_quad(whiteboard_full_mask)
-        for shape in shapes:
-            cv2.drawContours(frame_out, [shape], -1, (0, 0, 255), 2)
+            shapes = solve_mask_quad(whiteboard_full_mask)
+            for shape in shapes:
+                cv2.drawContours(frame_out, [shape], -1, (0, 0, 255), 2)
 
-        filename = os.path.basename(img_path)
-        out_path = os.path.join('./output', filename)
-        cv2.imwrite(out_path, frame_out)
-        print(f'Saved predicted output to {out_path}')
+            filename = os.path.basename(img_path)
+            out_path = os.path.join('./output', filename)
+            cv2.imwrite(out_path, frame_out)
+            print(f'Saved predicted output to {out_path}')
--- temporal_demo.py
+++ temporal_demo.py
@@ -1,6 +1,5 @@
 from torchvision import transforms as T
 import numpy as np
-import matplotlib
 import colorsys
 import network
 import torch
@@ -8,99 +7,119 @@ import time
 import cv2
 import os
 
+import matplotlib
+matplotlib.use('TkAgg')
+
 from place_solver import solve_mask_quad
 import config
 
-matplotlib.use('TkAgg')
+def overlay_image(image, pred):
+    # calc colorized prediction
+    colorized_pred = (color_mapping[pred,:]*255).astype('uint8')
+    colorized_pred = cv2.resize(colorized_pred, (cam_res[1], cam_res[0]), interpolation=cv2.INTER_NEAREST)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = network.modeling.deeplabv3plus_mobilenet(num_classes=config.NUM_CLASSES, output_stride=config.OUTPUT_STRIDE)
-if os.path.exists(f'models/cp_{config.MODEL_NAME}_{config.NUM_CLASSES}cls.pth'):
-    print(f'Loading pretrained weights from models/cp_{config.MODEL_NAME}_{config.NUM_CLASSES}cls.pth')
-    model.load_state_dict(torch.load(f'models/cp_{config.MODEL_NAME}_{config.NUM_CLASSES}cls.pth', map_location=device, weights_only=True))
-model.to(device)
+    # overlay the original image and the colorized prediction
+    overlay = cv2.addWeighted(image, 0.5, colorized_pred, 0.5, 0)
+    frame_out = cv2.cvtColor(overlay, cv2.COLOR_RGB2BGR)
 
-transform = T.Compose([
-    T.ToTensor(),   
-    T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
-])
+    # mask the vertex image with whiteboard mask
+    pred_cam_res = cv2.resize(pred, (cam_res[1], cam_res[0]), interpolation=cv2.INTER_NEAREST)
+    whiteboard_mask = (pred_cam_res == 1)
+    shapes = solve_mask_quad(whiteboard_mask)
+    for shape in shapes:
+        cv2.drawContours(frame_out, [shape], -1, (0, 0, 255), 2)
+    return frame_out
 
-save_period = 10
-pred_list = np.zeros((save_period, config.INPUT_SIZE[0], config.INPUT_SIZE[1]))
-next_index = 0
-frame_index = 0
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = network.modeling.deeplabv3plus_mobilenet(num_classes=config.NUM_CLASSES, output_stride=config.OUTPUT_STRIDE)
+    if os.path.exists(f'models/cp_{config.MODEL_NAME}_{config.NUM_CLASSES}cls.pth'):
+        print(f'Loading pretrained weights from models/cp_{config.MODEL_NAME}_{config.NUM_CLASSES}cls.pth')
+        model.load_state_dict(torch.load(f'models/cp_{config.MODEL_NAME}_{config.NUM_CLASSES}cls.pth', map_location=device, weights_only=True))
+    model.to(device)
 
-start_time = time.time()
-frames = []
-timestamps = []
+    transform = T.Compose([
+        T.ToTensor(),   
+        T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+    ])
 
-color_mapping = np.array([colorsys.hsv_to_rgb(i/config.NUM_CLASSES, 1, 1) for i in range(config.NUM_CLASSES)])
-cap = cv2.VideoCapture(-1)
-if not cap.isOpened():
-    print('failed to open webcam')
-    exit()
+    save_period = 10
+    pred_list = np.zeros((save_period, config.INPUT_SIZE[0], config.INPUT_SIZE[1]))
+    pred_prob_list = np.zeros((save_period, config.NUM_CLASSES, config.INPUT_SIZE[0], config.INPUT_SIZE[1]))
+    prev_pred = np.zeros((config.NUM_CLASSES, config.INPUT_SIZE[0], config.INPUT_SIZE[1]))
 
-with torch.no_grad():
-    model = model.eval()
-    while True:
-        ret, ci = cap.read()
-        if not ret:
-            print('failed to read webcam')
-            break
-        ci_rgb = cv2.cvtColor(ci, cv2.COLOR_BGR2RGB)
-        cam_res = ci_rgb.shape
+    next_index = 0
+    frame_index = 0
 
-        input_img = cv2.resize(ci_rgb, (config.INPUT_SIZE[1], config.INPUT_SIZE[0]))
-        img_tensor = transform(input_img).unsqueeze(0).to(device)
-        pred = model(img_tensor).max(1)[1].cpu().numpy()[0]
-        pred = np.array(pred)
+    start_time = time.time()
+    pred_frames = []
+    raw_frames = []
+    timestamps = []
 
-        # save to predlist
-        pred_list[frame_index%save_period] = pred
+    color_mapping = np.array([colorsys.hsv_to_rgb(i/config.NUM_CLASSES, 1, 1) for i in range(config.NUM_CLASSES)])
+    cap = cv2.VideoCapture(-1)
+    if not cap.isOpened():
+        print('failed to open webcam')
+        exit()
 
-        # filter for any pixel that was greater than 0 within n frames
-        has_whiteboard = (np.count_nonzero(pred_list > 0, axis=0) > 0).astype(int)
+    with torch.no_grad():
+        model = model.eval()
+        while True:
+            ret, ci = cap.read()
+            if not ret:
+                print('failed to read webcam')
+                break
+            ci_rgb = cv2.cvtColor(ci, cv2.COLOR_BGR2RGB)
+            cam_res = ci_rgb.shape
+            
+            input_img = cv2.resize(ci_rgb, (config.INPUT_SIZE[1], config.INPUT_SIZE[0]))
+            img_tensor = transform(input_img).unsqueeze(0).to(device)
+            pred = model(img_tensor)
+            pred_prob = torch.nn.functional.softmax(pred, dim=1)[0].cpu().numpy()
+            pred = pred.max(1)[1].cpu().numpy()[0]
+            pred = np.array(pred)
 
-        colorized_pred = (color_mapping[has_whiteboard,:]*255).astype('uint8')
-        colorized_pred = cv2.resize(colorized_pred, (cam_res[1], cam_res[0]), interpolation=cv2.INTER_NEAREST)
+            # save to predlist
+            pred_list[frame_index%save_period] = pred
+            pred_prob_list[frame_index%save_period] = pred_prob
 
-        # Overlay the original image and the colorized prediction
-        overlay = cv2.addWeighted(ci_rgb, 0.5, colorized_pred, 0.5, 0)
-        frame_out = cv2.cvtColor(overlay, cv2.COLOR_RGB2BGR)
+            prev_pred *= 0.8
+            prev_pred += pred_prob
 
-        # mask the vertex image with whiteboard mask
-        pred_cam_res = cv2.resize(has_whiteboard, (cam_res[1], cam_res[0]), interpolation=cv2.INTER_NEAREST)
-        whiteboard_mask = (pred_cam_res == 1)
+            # majority_vote = np.median(pred_list, axis=0).astype(np.int32)
+            temporal_pred = np.argmax(prev_pred, axis=0)
 
-        shapes = solve_mask_quad(whiteboard_mask)
-        for shape in shapes:
-            cv2.drawContours(frame_out, [shape], -1, (0, 0, 255), 2)
-        cv2.imshow('Camera', frame_out)
+            frame_out = overlay_image(ci_rgb, temporal_pred)
 
-        frames.append(frame_out)
-        timestamps.append(time.time()-start_time)
+            cv2.imshow('Camera', frame_out)
 
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord(' '):
-            while os.path.exists(f'res/image{nextIndex}.png'): nextIndex += 1
-            cv2.imwrite(f'res/image{nextIndex}.png', ci)
-            print(f'Saved res/image{nextIndex}.png')
-        if key == ord('q'):
-            break
-        frame_index += 1
-cv2.destroyAllWindows()
+            pred_frames.append(frame_out)
+            raw_frames.append(ci)
+            timestamps.append(time.time()-start_time)
 
-fps = 60
-frame_time = 1/fps
-frame_durations = [t - s for s, t in zip(timestamps, timestamps[1:])]
-frame_durations.append(frame_durations[-1])
-video = cv2.VideoWriter('output/output.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (frames[0].shape[1], frames[0].shape[0]))
-next_frame = 1
-running = frame_durations[0]
-elapsed = 0
-while next_frame < len(frame_durations):
-    video.write(frames[next_frame-1])
-    elapsed += frame_time
-    if elapsed >= running:
-        running += frame_durations[next_frame]
-        next_frame += 1
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord(' '):
+                while os.path.exists(f'res/image{nextIndex}.png'): nextIndex += 1
+                cv2.imwrite(f'res/image{nextIndex}.png', ci)
+                print(f'Saved res/image{nextIndex}.png')
+            if key == ord('q'):
+                break
+            frame_index += 1
+    cv2.destroyAllWindows()
+
+    fps = 30
+    frame_time = 1/fps
+    frame_durations = [t - s for s, t in zip(timestamps, timestamps[1:])]
+    frame_durations.append(frame_durations[-1])
+    pred_video = cv2.VideoWriter('output/pred_output.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (pred_frames[0].shape[1], pred_frames[0].shape[0]))
+    raw_video = cv2.VideoWriter('output/raw_output.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (raw_frames[0].shape[1], raw_frames[0].shape[0]))
+    next_frame = 1
+    running = frame_durations[0]
+    elapsed = 0
+    while next_frame < len(frame_durations):
+        pred_video.write(pred_frames[next_frame-1])
+        raw_video.write(raw_frames[next_frame-1])
+        elapsed += frame_time
+        if elapsed >= running:
+            running += frame_durations[next_frame]
+            next_frame += 1
